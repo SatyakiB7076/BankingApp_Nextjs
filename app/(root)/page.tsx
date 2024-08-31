3647ad816444e71cae490bf700713d400f72f0ff
Satyaki Bhattacharjee <140399051+SatyakiB7076@users.noreply.github.com>
2024-09-01 00:44:38 +0530

    implement two new component one headerbox and totalbalancebox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn={firstName:'Satyaki'}
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={3500.67}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
