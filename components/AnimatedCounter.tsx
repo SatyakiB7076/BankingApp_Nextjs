a24146f6f3821e8bc7527e78d4c32a399e16b763
Satyaki Bhattacharjee <140399051+SatyakiB7076@users.noreply.github.com>
2024-09-01 11:33:26 +0530

    add the doughnut chart and countup method in balance section

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,17 @@
+'use client';
+import React from 'react'
+import CountUp from 'react-countup';
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+    return (
+        <p className='w-full'>
+            <CountUp
+                decimals={2}
+                decimal="."
+                prefix="₹ "
+                end={amount} />
+        </p>
+    )
+}
+
+export default AnimatedCounter
